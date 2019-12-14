7dd2efa5d50c40ba7e3b8446cc0a5c9245344f0d
Lionel Salgado <liosalgado@alumnos.uvigo.es>
2019-12-15 00:55:40 +0100

    parajesus

--- app/src/main/java/com/example/gamegui/InGame.java
+++ app/src/main/java/com/example/gamegui/InGame.java
@@ -24,7 +24,6 @@ public class InGame extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_in_game);
         final int nronda = 0;
         //Creamos os xogadores
